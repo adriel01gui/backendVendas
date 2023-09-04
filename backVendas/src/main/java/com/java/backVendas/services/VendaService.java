623b4dce2ce5c85c5acfac8a531759c08fd02c54
Adriel Guimaraes <adrielguimaraes01@gmail.com>
2023-09-03 22:26:13 -0300

    criação dos enums

--- backVendas/src/main/java/com/java/backVendas/services/VendaService.java
+++ backVendas/src/main/java/com/java/backVendas/services/VendaService.java
@@ -1,7 +1,19 @@
 package com.java.backVendas.services;
 
+import com.java.backVendas.repositories.ItensVendaRepository;
+import com.java.backVendas.repositories.VendaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VendaService {
+
+    @Autowired
+    private VendaRepository repository;
+
+    @Autowired
+    private ItensVendaRepository itensVendaRepository;
+
+    ModelMapper modelMapper = new ModelMapper();
 }
---
+++ backVendas/src/main/java/enums/CategoriaEnum.java
@@ -0,0 +1,16 @@
+package enums;
+
+public enum CategoriaEnum {
+    BEBIDA("Bebida"),
+    COMIDA("Comida");
+
+    private final String descricao;
+
+    CategoriaEnum(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+}
